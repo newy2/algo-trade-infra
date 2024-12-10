739bdab21c36f04603b5f84974bb9845bee5232e
Jonghoon Yoon <by.newy@gmail.com>
2024-12-10 23:31:41 +0900

    #32 refector: 프론트엔드 인프라 클래스 선언

---
+++ frontend_infra/FrontendInfra.ts
@@ -0,0 +1,26 @@
+import S3Info from "./s3/S3Info";
+import FrontendCloudFrontInfo from "./cloudfront/FrontendCloudFrontInfo";
+import FrontendLambdaInfo from "./lambda/FrontendLambdaInfo";
+import SnsInfo from "./sns/SnsInfo";
+import FrontendSsmInfo from "./ssm/FrontendSsmInfo";
+import FrontendIamInfo from "./iam/FrontendIamInfo";
+import CommonIamInfo from "../common_infra/iam/CommonIamInfo";
+import CommonLambdaInfo from "../common_infra/lambda/CommonLambdaInfo";
+
+export default class FrontendInfra {
+  constructor(
+    commonIamInfo: CommonIamInfo,
+    commonLambdaInfo: CommonLambdaInfo,
+  ) {
+    const s3Info = new S3Info();
+    const frontendIamInfo = new FrontendIamInfo(commonIamInfo.commonPolicyInfo);
+    const frontendCloudfrontInfo = new FrontendCloudFrontInfo(s3Info);
+    const frontendLambdaInfo = new FrontendLambdaInfo(
+      frontendIamInfo,
+      s3Info,
+      commonLambdaInfo,
+    );
+    new SnsInfo(frontendLambdaInfo);
+    new FrontendSsmInfo(frontendCloudfrontInfo);
+  }
+}
--- index.ts
+++ index.ts
@@ -1,47 +1,30 @@
 import VpcInfo from "./aws/vpc/VpcInfo";
 import IamInfo from "./aws/iam/IamInfo";
 import CommonIamInfo from "./common_infra/iam/CommonIamInfo";
-import FrontendIamInfo from "./frontend_infra/iam/FrontendIamInfo";
 import EventBridgeInfo from "./aws/event_bridge/EventBridgeInfo";
 import SsmInfo from "./aws/ssm/SsmInfo";
-import FrontendSsmInfo from "./frontend_infra/ssm/FrontendSsmInfo";
 import EcrInfo from "./aws/ecr/EcrInfo";
 import LambdaInfo from "./aws/lambda/LambdaInfo";
 import CommonLambdaInfo from "./common_infra/lambda/CommonLambdaInfo";
-import FrontendLambdaInfo from "./frontend_infra/lambda/FrontendLambdaInfo";
 import { RdsInfo } from "./aws/rds/RdsInfo";
-import S3Info from "./frontend_infra/s3/S3Info";
 import CloudFrontInfo from "./aws/cloudfront/CloudFrontInfo";
-import FrontendCloudFrontInfo from "./frontend_infra/cloudfront/FrontendCloudFrontInfo";
 import SqsInfo from "./aws/sqs/SqsInfo";
-import SnsInfo from "./frontend_infra/sns/SnsInfo";
 import Ec2Info from "./aws/ec2/Ec2Info";
-
-const vpcInfo = new VpcInfo();
+import FrontendInfra from "./frontend_infra/FrontendInfra";
 
 const commonIamInfo = new CommonIamInfo();
-const iamInfo = new IamInfo(commonIamInfo.commonPolicyInfo);
-const frontendIamInfo = new FrontendIamInfo(commonIamInfo.commonPolicyInfo);
+const commonLambdaInfo = new CommonLambdaInfo();
 
+const vpcInfo = new VpcInfo();
+const iamInfo = new IamInfo(commonIamInfo.commonPolicyInfo);
 const ecrInfo = new EcrInfo();
 const sqsInfo = new SqsInfo();
-const s3Info = new S3Info();
 const rdsInfo = new RdsInfo(vpcInfo);
 new Ec2Info(vpcInfo, iamInfo);
 const cloudfrontInfo = new CloudFrontInfo();
-const frontendCloudfrontInfo = new FrontendCloudFrontInfo(s3Info);
-
-const commonLambdaInfo = new CommonLambdaInfo();
 const lambdaInfo = new LambdaInfo(iamInfo, commonLambdaInfo);
-const frontendLambdaInfo = new FrontendLambdaInfo(
-  frontendIamInfo,
-  s3Info,
-  commonLambdaInfo,
-);
-
-new SnsInfo(frontendLambdaInfo);
 new EventBridgeInfo(ecrInfo, lambdaInfo);
-
 // TODO Refector: 각 Info 에서 SsmInfo 를 호출하도록 할까?
 new SsmInfo(vpcInfo, ecrInfo, rdsInfo, cloudfrontInfo, sqsInfo);
-new FrontendSsmInfo(frontendCloudfrontInfo);
+
+new FrontendInfra(commonIamInfo, commonLambdaInfo);
