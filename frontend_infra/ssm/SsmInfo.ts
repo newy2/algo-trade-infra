463f14e4f82b961cfd34446991e4b70754ba9b9b
Jonghoon Yoon <by.newy@gmail.com>
2024-12-11 00:11:44 +0900

    #32 refector: SsmInfo 필드 제거

--- frontend_infra/ssm/SsmInfo.ts
+++ frontend_infra/ssm/SsmInfo.ts
@@ -2,9 +2,7 @@ import CloudFrontInfo from "../cloudfront/CloudFrontInfo";
 import ParameterStoreInfo from "./application_management/ParameterStoreInfo";
 
 export default class SsmInfo {
-  private readonly parameterStoreInfo: ParameterStoreInfo;
-
   constructor(cloudFrontInfo: CloudFrontInfo) {
-    this.parameterStoreInfo = new ParameterStoreInfo(cloudFrontInfo);
+    new ParameterStoreInfo(cloudFrontInfo);
   }
 }
