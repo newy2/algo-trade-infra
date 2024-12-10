90a31a51745890ad391d30acc5d2902e64521f7a
Jonghoon Yoon <by.newy@gmail.com>
2024-12-11 01:09:57 +0900

    #32 refector: 백엔드 EventBridgeInfo 필드 제거

--- aws/event_bridge/EventBridgeInfo.ts
+++ aws/event_bridge/EventBridgeInfo.ts
@@ -3,9 +3,7 @@ import EcrInfo from "../ecr/EcrInfo";
 import LambdaInfo from "../lambda/LambdaInfo";
 
 export default class EventBridgeInfo {
-  private readonly ruleInfo: RuleInfo;
-
   constructor(ecrInfo: EcrInfo, lambdaInfo: LambdaInfo) {
-    this.ruleInfo = new RuleInfo(ecrInfo, lambdaInfo);
+    new RuleInfo(ecrInfo, lambdaInfo);
   }
 }
